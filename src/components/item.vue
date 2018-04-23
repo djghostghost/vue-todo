e7846dd39e50f0f73bee59b35ee1996649a7d2ed
djghostghost <xuxi746@163.com>
2018-04-23 18:08:40 +0900

    Initial commit

---
+++ src/components/item.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="list-item editingClass editing">
+      <lable class="checkbox">
+          <input type="checkbox" v-model="item.checked">
+          <span class="checkbox-custom"></span>
+      </lable>
+      <input type="text" v-model="item.text" placeholder="input something..."/>
+      <a class="delete-item">
+          <span class="icon-trash"></span>
+      </a>
+  </div>
+</template>
+<script>
+export default {
+  props:['item']
+}
+</script>
+<style lang="less">
+@import '../common/style/list-items.less';
+</style>
+
